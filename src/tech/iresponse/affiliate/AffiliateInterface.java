b6db4856d6ae6cd0ec4665e33b8cb931a7452776
Amine Bureau <malohtie@gmail.com>
2022-04-03 16:15:56 +0000

    Init

---
+++ src/tech/iresponse/affiliate/AffiliateInterface.java
@@ -0,0 +1,19 @@
+package tech.iresponse.affiliate;
+
+import java.util.List;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import tech.iresponse.models.affiliate.Offer;
+
+public interface AffiliateInterface {
+
+    //void getOffers(List offersIdsList) throws Exception;
+    //String getSuppressionLink(Offer offer) throws Exception;
+    //JSONArray getClicks(String startDate, String endDate) throws Exception;
+    //JSONArray getConversions(String startDate, String endDate) throws Exception;
+
+    public void getOffers(List<Integer> offersIds) throws Exception;
+    public String getSuppressionLink(Offer offer) throws Exception;
+    public JSONArray getClicks(String startDate,String endDate) throws Exception;
+    public JSONArray getConversions(String startDate,String endDate) throws Exception;
+}
---
+++ src/tech/iresponse/logging/Loggers.java
@@ -0,0 +1,11 @@
+package tech.iresponse.logging;
+
+import org.apache.commons.lang.exception.ExceptionUtils;
+import org.apache.log4j.Logger;
+
+public class Loggers {
+    public static void error(Throwable th) {
+        Logger.getLogger("rootLogger").error(ExceptionUtils.getFullStackTrace(th));
+        //Logger.getLogger("rootLogger").error(ExceptionUtils.getStackTrace(th));
+    }
+}
